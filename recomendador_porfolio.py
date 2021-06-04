0923097cb43a367168fcadeaf8aa0e047dd30a2d
Constanza Córdova <82466247+constanzacordova@users.noreply.github.com>
2021-06-03 22:29:50 -0400

    Update recomendador_porfolio.py

--- recomendador_porfolio.py
+++ recomendador_porfolio.py
@@ -93,7 +93,7 @@ def main():
     menu = st.sidebar.selectbox("", (op1, op2, op3))
 
     if menu == op1:
-        st.title("APP - Recomendador de Portafolios")
+        st.title("Recomendador de Portafolios de Inver$ión")
         video_file = open('apoyo/video_inicio.mp4', 'rb')
         video_bytes = video_file.read()
         st.video(video_bytes, start_time = 0)
